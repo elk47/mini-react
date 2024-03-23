c740408390db87cfe6c3ddafcb17e16c522a9df6
elk47 <meuu407@gmail.com>
2024-03-23 12:26:40 +0800

    refactor

--- main.js
+++ main.js
@@ -1,46 +1,4 @@
-const createTextNode = (val) => {
-    return {
-        type: "TEXT_NODE",
-        props: {
-            nodeValue: val,
-            children: []
-        }
-    }
-}
+import ReactDOM from "./core/ReactDom.js"
+import App from './App.js'
 
-const createElement = (type, props, ...children) => {
-    return {
-        type,
-        props: {
-            ...props,
-            children
-        }
-    }
-}
-
-const render = (element, container) => {
-    const dom = element.type === "TEXT_NODE"
-        ? document.createTextNode(element.props.nodeVal)
-        : document.createElement(element.type)
-    Object.keys(element.props).forEach((key) => {
-        if (key !== "children") {
-            dom[key] = element.props[key]
-        }
-    })
-    element.props.children.forEach(child => {
-        render(child, dom)
-    })
-    console.log(dom)
-    container.append(dom)
-}
-
-const props = {
-    id: "app"
-}
-const textnode = createTextNode('app')
-const vdom = createElement("div", props, textnode)
-
-const dom = document.createElement(vdom.type)
-dom.id = vdom.props.id
-const root = document.querySelector("#root")
-render(vdom, root)
+ReactDOM.createRoot(document.getElementById("root")).render(App)
