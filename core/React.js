76feaacb241222217d68ae13a86837bcb92c6a69
Sarah Thompson <138575072+elk47@users.noreply.github.com>
2024-03-24 07:40:00 +0000

    fiber

--- core/React.js
+++ core/React.js
@@ -19,21 +19,69 @@ const createElement = (type, props, ...children) => {
     },
   };
 };
+let nextWork = null;
 
 const render = (element, container) => {
-  const dom =
-    element.type === "TEXT_NODE"
-      ? document.createTextNode("")
-      : document.createElement(element.type);
-  Object.keys(element.props).forEach((key) => {
-    if (key !== "children") {
-      dom[key] = element.props[key];
+  nextWork = {
+    dom: container,
+    props: {
+      children: [element],
+    },
+    parent: null,
+    child: null,
+    sibling: null,
+  };
+  requestIdleCallback(workLoop);
+};
+
+function workLoop(deadline) {
+  let shouldYield = false;
+
+  while (!shouldYield && nextWork) {
+    nextWork = performUnitOfWork(nextWork);
+    shouldYield = deadline.timeRemaining() < 1;
+  }
+}
+
+const performUnitOfWork = (work) => {
+  if (!work.dom) {
+    const dom = (work.dom =
+      work.type === "TEXT_NODE"
+        ? document.createTextNode("")
+        : document.createElement(work.type));
+    work.parent.dom.append(dom);
+    Object.keys(work.props).forEach((key) => {
+      if (key !== "children") {
+        dom[key] = work.props[key];
+      }
+    });
+  }
+  const children = work.props.children;
+  let prevChild = null;
+  children.forEach((child) => {
+    const newChild = {
+      type: child.type,
+      props: child.props,
+      parent: work,
+      child: null,
+      sibling: null,
+      dom: null,
+    };
+    if (!prevChild) {
+      work.child = newChild;
+    } else {
+      prevChild.sibling = newChild;
     }
+    prevChild = newChild;
   });
-  element.props.children.forEach((child) => {
-    render(child, dom);
-  });
-  container.append(dom);
+  if (work.child) {
+    return work.child;
+  }
+  if (work.sibling) {
+    return work.sibling;
+  }
+
+  return work.parent.sibling;
 };
 
 const React = {
